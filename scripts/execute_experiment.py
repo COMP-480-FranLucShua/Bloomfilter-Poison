dee01bc9aed0dcff2dce7feb0c65e9588e71a489
jgr142 <joshuagisiger142@gmail.com>
2025-12-12 23:22:06 -0600

    fix: fix arrays

--- scripts/execute_experiment.py
+++ scripts/execute_experiment.py
@@ -23,7 +23,7 @@ def load_dataset(path: str):
 
 def run_pipeline(system, test_sample, attacker, attack_size):
     attacker.attack(attack_size)
-    return system.query_array(test_sample)
+    return system.query_array(DataArray.from_array(test_sample))
 
 def assemble_system(buckets: int, num_hfs: int, seed: int, init_insert_size: int, rng: RandomNumberGenerator, urls) -> SystemEmulator:
     bf: BloomFilter = BloomFilter(buckets, num_hfs, rng)
@@ -32,12 +32,12 @@ def assemble_system(buckets: int, num_hfs: int, seed: int, init_insert_size: int
     return system
 
 def assemble_naive_attacker(sys: SystemEmulator, rng: RandomNumberGenerator, urls) -> NaiveAttacker:
-    sampler: StringSampler = StringSampler(urls, rng)
+    sampler: StringSampler = StringSampler(StringArray(urls), rng)
     attacker: NaiveAttacker = NaiveAttacker(sys, sampler)
     return attacker
 
 def assemble_timing_attacker(sys: SystemEmulator, rng: RandomNumberGenerator, urls) -> TimingAttacker:
-    sampler: StringSampler = StringSampler(urls, rng)
+    sampler: StringSampler = StringSampler(StringArray(urls), rng)
     attacker: TimingAttacker = TimingAttacker(sys, sampler, 0.15)
     return attacker
 
