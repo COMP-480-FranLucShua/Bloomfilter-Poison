941e4a94d8e17218e508176f331be8957ef1bc06
Luca Chackalackal <lmc16@rice.edu>
2025-12-12 23:09:49 -0600

    timing attacker

--- scripts/c_modules/timing_attacker.py
+++ scripts/c_modules/timing_attacker.py
@@ -1,5 +1,5 @@
 import ctypes
-from ctypes import c_void_p, POINTER, c_size_t
+from ctypes import c_void_p, POINTER, c_size_t, c_double
 
 from ._lib import lib
 from ._c_object import _C_Object
@@ -18,25 +18,27 @@ class Sampler(Protocol):
     _interface: SamplerInterface
     _c_obj: c_void_p
 
-lib.naive_attacker_create.argtypes = [POINTER(SystemInterface),
+lib.timing_attacker_create.argtypes = [POINTER(SystemInterface),
                                       c_void_p,
                                       POINTER(SamplerInterface),
-                                      c_void_p]
-lib.naive_attacker_create.restype = c_void_p
+                                      c_void_p,
+                                      c_double]
+lib.timing_attacker_create.restype = c_void_p
 
-lib.naive_attacker_destroy.argtypes = [c_void_p]
-lib.naive_attacker_destroy.restype = None
+lib.timing_attacker_create.argtypes = [c_void_p]
+lib.timing_attacker_create.restype = None
 
-class NaiveAttacker(_C_Object):
-    def __init__(self, sys: System, sampler: Sampler):
+class TimingAttacker(_C_Object):
+    def __init__(self, sys: System, sampler: Sampler, training_proportion: float):
         assert isinstance(sys, System)
         assert isinstance(sampler, Sampler)
 
-        c_obj = lib.naive_attacker_create(sys._interface, sys._c_obj,
-                                          sampler._interface, sampler._c_obj)
+        c_obj = lib.timing_attacker_create(sys._interface, sys._c_obj,
+                                          sampler._interface, sampler._c_obj,
+                                          c_double(training_proportion))
         
-        super().__init__("naive-attacker", c_obj, lib.naive_attacker_destroy)
-        self._interface = ctypes.pointer(AttackerInterface.in_dll(lib, "naive_attacker_interface"))
+        super().__init__("timing-attacker", c_obj, lib.timing_attacker_destroy)
+        self._interface = ctypes.pointer(AttackerInterface.in_dll(lib, "timing_attacker_interface"))
     
     def attack(self, attack_size: int):
         self._interface.contents.attack(self._c_obj, c_size_t(attack_size))
