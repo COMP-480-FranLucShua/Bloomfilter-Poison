3a08fdfef9cddb4abc7a7ea9291d97b387e69f86
Luca Chackalackal <lmc16@rice.edu>
2025-12-12 19:26:34 -0600

    added docstring

--- clib/include/interfaces/attacker.h
+++ clib/include/interfaces/attacker.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+/**
+ * Interface for an attacker
+ * 
+ * An attacker's attack function takes a parameter attack_size
+ * 
+ * An attacker may insert at most attack_size values into its system,
+ * with the goal of maximizing the false positive rate
+ */
 typedef struct {
 	void (*attack)(void *self, size_t attack_size);
 } Attacker;
